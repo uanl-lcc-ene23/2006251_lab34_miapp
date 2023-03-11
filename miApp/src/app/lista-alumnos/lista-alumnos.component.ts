8b86165e3ec7a454a9ae2e5040a9155bc7539eac
pedrogzz18 <pedro.gonzalezot@uanl.edu.mx>
2023-03-11 12:20:51 -0600

    practica_6

---
+++ miApp/src/app/lista-alumnos/lista-alumnos.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lista-alumnos',
+  templateUrl: './lista-alumnos.component.html',
+  styleUrls: ['./lista-alumnos.component.css']
+})
+export class ListaAlumnosComponent {
+
+  alumnos: any = ["Carlos", "Ximena", "Alberto", "Eduardo", "Baraba", "Georgina", "Kevin"];
+
+}
