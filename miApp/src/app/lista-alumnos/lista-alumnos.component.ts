329a043da14ce1af007c028c08b5735b97f9316a
pedrogzz18 <pedro.gonzalezot@uanl.edu.mx>
2023-04-01 23:49:20 -0600

    practica_7

--- miApp/src/app/lista-alumnos/lista-alumnos.component.ts
+++ miApp/src/app/lista-alumnos/lista-alumnos.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ActionSheetController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-lista-alumnos',
@@ -6,7 +8,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./lista-alumnos.component.css']
 })
 export class ListaAlumnosComponent {
-
   alumnos: any = ["Carlos", "Ximena", "Alberto", "Eduardo", "Barbara", "Georgina", "Kevin"];
+  nombre: any = "Pedro";
+
+  constructor(private actionSheetCtrl: ActionSheetController) {}
+
+  async presentarActionSheet() {
+    const actionSheet = await this.actionSheetCtrl.create({
+      header: 'Confirma que desea eliminar este registro',
+      //subHeader: "Confirma que quieres eliminar este registro",
+      buttons: [
+        {
+          text: 'Eliminar',
+          role: 'destructive',
+          data: {
+            action: 'delete',
+          },
+        },
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          data: {
+            action: 'cancel',
+          },
+        },
+      ],
+    });
+
+    await actionSheet.present();
+  }
+
+
+  
 
 }
